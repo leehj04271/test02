0c2494851c7ed7d25d8bee41c228ac325f062981
leehj04271 <122263151+leehj04271@users.noreply.github.com>
2023-03-13 19:29:04 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@
     $("#nav").load("nav.html .nav-box", function () {
 
 
-        const loc = document.location.pathname.slice(1); const list = document.querySelectorAll('#nav .list');
+        const loc = document.location.pathname.slice(8); const list = document.querySelectorAll('#nav .list');
 
 
         list.forEach((item) => {
